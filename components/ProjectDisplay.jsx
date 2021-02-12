1b837c7da2c5da92f3274afddca2223a0d15ad4a
Aaron <parisi.aaron@gmail.com>
2021-02-12 13:25:54 -0800

    more styling for project displays; tech lists will not align correctly

--- components/ProjectDisplay.jsx
+++ components/ProjectDisplay.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 
-const ProjectDisplay = ({ projTitle, imageUrl, url }) => {
+const ProjectDisplay = ({ projTitle, imageUrl, url, description, techs }) => {
   return (
     <div className="project-display">
       <div className="project-thumbnail">
         <a href={url}><img src={imageUrl} alt={`web app thumbnail for ${projTitle}`}/></a>
       </div>
+      
       <div className="project-info">
         <h2>{projTitle}</h2>
+
+        <div className="project-details">
+          <p className="project-description">
+            {description}
+          </p>
+          
+          <p className="techs">
+            {techs.join(" | ")}
+          </p>
+        </div>
       </div>
     </div>
   )
--- content.jsx
+++ content.jsx
@@ -8,9 +8,27 @@ const Content = () => {
     <div className="content">
       <NavBar />
       <div className="project-displays">
-        <ProjectDisplay projTitle="Towers of Hanoi" imageUrl="./thumbnails/Towers.png" url="https://towers-of-hanoi-e1df0.web.app/"/>
-        <ProjectDisplay projTitle="Todos" imageUrl="./thumbnails/Todos.png" url=""/>
-        <ProjectDisplay projTitle="TicTacToe" imageUrl="./thumbnails/TicTacToe.png" url="https://tictactoe-32141.web.app/"/>
+        <ProjectDisplay
+          projTitle="Towers of Hanoi"
+          imageUrl="./thumbnails/Towers.png"
+          url="https://towers-of-hanoi-e1df0.web.app/"
+          description="Are you smarter than a boot-camper? See if you can complete this classic game in fewer moves than the recursive AI."
+          techs={["jQuery"]}
+        />
+        <ProjectDisplay
+          projTitle="Todos"
+          imageUrl="./thumbnails/Todos.png"
+          url=""
+          description="Overwhelmed by your galactic conquests? Keep track of your tasks with this easy-to-use todo list featuring task-reordering and color-coded completion. It is your destiny."
+          techs={["Rails", "React", "Redux", "PostgreSQL"]}
+        />
+        <ProjectDisplay
+          projTitle="TicTacToe"
+          imageUrl="./thumbnails/TicTacToe.png"
+          url="https://tictactoe-32141.web.app/"
+          description="No more wasted paper or yucky chalk dust with this sleek, reset-able rendition of your favorite childhood game. No cheating allowed (or possible)."
+          techs={["Ruby", "JavaScript"]}
+        />
       </div>
     </div>
   )
