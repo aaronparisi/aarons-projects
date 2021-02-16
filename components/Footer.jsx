cd75c72445f8c4a1a3eb13179f30a6eeefd8d41c
Aaron <parisi.aaron@gmail.com>
2021-02-16 12:05:33 -0800

    icon attribution

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <p className="footer">Design inspired by <a href="https://brittanychiang.com/">Brittany Chiang</a> | Blinking Cursor inspired by <a href="Artem Gordinsky">Artem Gordinsky</a></p>
+    <p className="footer">Design inspired by <a href="https://brittanychiang.com/">Brittany Chiang</a> | Blinking Cursor inspired by <a href="Artem Gordinsky">Artem Gordinsky</a> | Icons curtesy of <a href="https://icons8.com/icons/">Icons8</a></p>
   )
 }
 
