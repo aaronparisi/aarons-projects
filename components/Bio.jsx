f694dd1fb8c1f7bd6262238afbc9082c7e5bf2c1
Aaron <parisi.aaron@gmail.com>
2021-02-16 14:47:51 -0800

    some styling changes, about section shortening

--- components/Bio.jsx
+++ components/Bio.jsx
@@ -21,14 +21,13 @@ const Bio = () => {
         <h3 id="about-header">About me:</h3>
         <div id="about-content">
           <p id="about-paragraph">
-            After graduating with a degree in mathematics from <a href="https://www.hartwick.edu/">Hartwick College</a> with degrees in Mathematics and Economics in 2014,
-            I taught AP Calculus BC, Multivariable Calculus, and Statistics at <a href="https://www.salisburyschool.org/">Salisbury School</a>.
-            Although I loved helping others understand difficult concepts, I missed creating and expressing ideas of my own.
-            That plus the rural Connecticut winters made me want to find new challenges...
-            After five years of tinkering with programming here and there, I was working
-            a job at a <a href="https://www.1031services.com/">1031 exchange company</a>.
-            I was able to use <a href="https://support.microsoft.com/en-us/office/quick-start-create-a-macro-741130ca-080d-49f5-9471-1e5fb3d581a8">Excel Macros</a> and <a href="https://www.autohotkey.com/">AutoHotkey</a> to automate and streamline a few things, but I wasn't able to actually change the software we were using.
-            I realized making software more intuitive and user-friendly was a passion.  With the help of <a href="https://open.appacademy.io/">App Academy Open</a>,
+            I started off teaching AP Calculus BC, Multivariable Calculus, and Statistics at <a href="https://www.salisburyschool.org/">Salisbury School</a> on a whiteboard...
+            <br/>
+            <br/>
+            Then I worked a job at a <a href="https://www.1031services.com/">1031 real estate exchange company</a> whose database software had some... quirks...
+            <br/>
+            <br/>
+            I finally realized making software more intuitive and user-friendly is a passion.  With the help of <a href="https://open.appacademy.io/">App Academy Open</a>,
             I've been able to teach myself the skills to do that.
           </p>
           <div id="headshot-container"><img id="headshot" src="./misc_images/headshot.png" alt="Headshot of Aaron Parisi"/></div>
