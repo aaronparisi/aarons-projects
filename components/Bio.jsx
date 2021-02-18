e68852a73be26c13ab9276185c68e84c74ff242c
Aaron <parisi.aaron@gmail.com>
2021-02-17 19:45:36 -0800

    changed bio language a little

--- components/Bio.jsx
+++ components/Bio.jsx
@@ -21,14 +21,14 @@ const Bio = () => {
         <h3 id="about-header">About me:</h3>
         <div id="about-content">
           <p id="about-paragraph">
-            I started off teaching AP Calculus BC, Multivariable Calculus, and Statistics at <a href="https://www.salisburyschool.org/">Salisbury School</a> on a whiteboard...
+            It started when I was teaching AP Calculus BC, Multivariable Calculus, and Statistics at <a href="https://www.salisburyschool.org/">Salisbury School</a> on a whiteboard...
             <br/>
             <br/>
             Then I worked a job at a <a href="https://www.1031services.com/">real estate exchange company</a> whose database software had some... quirks...
             <br/>
             <br/>
-            I finally realized making software more intuitive and user-friendly is a passion.  With the help of <a href="https://open.appacademy.io/">App Academy Open</a>,
-            I've been able to teach myself the skills to do that.
+            Finally it dawned on me - I want to make software that is easy to use, and that *gasp* looks nice.  With the help of <a href="https://open.appacademy.io/">App Academy Open</a>,
+            I've been able to teach myself to do that.
           </p>
           <div id="headshot-container"><img id="headshot" src="./misc_images/headshot.png" alt="Headshot of Aaron Parisi"/></div>
         </div>
