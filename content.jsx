b866aeefb5b2daaccc4c37e14cb6b68965dbb9f9
Aaron <parisi.aaron@gmail.com>
2021-02-17 19:34:06 -0800

    added https to project urls

--- content.jsx
+++ content.jsx
@@ -18,7 +18,7 @@ const Content = () => {
           projTitle="Towers of Hanoi"
           imageUrl="./thumbnails/Towers.png"
           // pageUrl="https://towers-of-hanoi-e1df0.web.app/"
-          pageUrl="www.towers.aaronparisidev.com"
+          pageUrl="https://towers.aaronparisidev.com"
           gitUrl="https://github.com/aaronparisi/Towers-of-Hanoi-jQuery"
           description="Are you smarter than a boot-camper? See if you can complete this classic game in fewer moves than the recursive AI."
           techs={["jQuery"]}
@@ -28,7 +28,7 @@ const Content = () => {
           imageUrl="./thumbnails/Todos.png"
           // pageUrl="https://www.todos.aaronparisidev.com"
           // pageUrl="https://aarons-todos.herokuapp.com/"
-          pageUrl="www.todos.aaronparisidev.com"
+          pageUrl="https://www.todos.aaronparisidev.com"
           gitUrl="https://github.com/aaronparisi/Todo-List-Rails-React-Redux"
           description="Overwhelmed by your galactic conquests? Keep track of your tasks with this easy-to-use todo list featuring task-reordering and color-coded completion. It is your destiny."
           techs={["Rails", "React", "Redux", "PostgreSQL"]}
@@ -37,7 +37,7 @@ const Content = () => {
           projTitle="TicTacToe"
           imageUrl="./thumbnails/TicTacToe.png"
           // pageUrl="https://tictactoe-32141.web.app/"
-          pageUrl="www.tictactoe.aaronparisidev.com"
+          pageUrl="https://www.tictactoe.aaronparisidev.com"
           gitUrl="https://github.com/aaronparisi/TicTacToe"
           description="No more wasted paper or yucky chalk dust with this sleek, reset-able rendition of your favorite childhood game. No cheating allowed (or possible)."
           techs={["Ruby", "JavaScript"]}
