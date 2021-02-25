08cf784c169b4280a6b667d6875652b5e017e65e
Aaron <parisi.aaron@gmail.com>
2021-02-25 20:35:37 +0000

    made cursor blink with jquery

---
+++ blinking_cursor.js
@@ -0,0 +1,14 @@
+let $cursor = $('#blinking-cursor')
+
+const timeoutLoop = () => {
+  setTimeout(() => {
+    $('#blinking-cursor').toggleClass('blinked-on')
+    $('#blinking-cursor').toggleClass('blinked-off')
+
+    timeoutLoop()
+  }, 500);
+}
+
+$(document).ready(() => {
+  timeoutLoop()
+})
